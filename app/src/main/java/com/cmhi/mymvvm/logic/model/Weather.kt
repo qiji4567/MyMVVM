bd379cc07773652c44ac5bde63b0fb31ae64e9ca
qiji <qiji@qq.com>
2021-08-19 14:14:26 +0800

    修改包名

--- app/src/main/java/com/cmhi/mymvvm/logic/model/Weather.kt
+++
@@ -1,8 +0,0 @@
-package com.cmhi.mymvvm.logic.model
-
-import com.cmhi.mymvvm.logic.model.DailyResponse
-import com.cmhi.mymvvm.logic.model.RealtimeResponse
-
-class Weather(val realtime: RealtimeResponse.Realtime,val daily: DailyResponse.Daily) {
-
-}
